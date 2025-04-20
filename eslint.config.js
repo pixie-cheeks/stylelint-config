d1397b30af3fdb6bf236a26f422d76465425d1d6
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-04-20 22:11:43 +0530

    Update packages successfully

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,11 @@
 import pixie from '@pixie-cheeks/eslint-config';
 
-export default [...pixie.base, pixie.prettier];
+export default [
+  ...pixie.base,
+  {
+    rules: {
+      'import-x/no-default-export': 'off',
+    },
+  },
+  pixie.prettier,
+];
